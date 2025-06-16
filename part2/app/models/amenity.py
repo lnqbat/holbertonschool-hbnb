83ec5e45cba9a2d00c741ecbb5272019cb15f236
lnqbat <b.lonqueu98@gmail.com>
2025-06-16 10:27:40 +0200

    feat: implement Amenity class with validation for name

---
+++ part2/app/models/amenity.py
@@ -0,0 +1,11 @@
+from models import BaseModel
+
+class Amenity(BaseModel):
+    
+    def __init__(self, name):
+        """
+        Amenity class.
+        """
+        if not name or len(name) > 50:
+            raise ValueError("Invalid amenity name")
+        self.name = name
