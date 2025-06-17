816f181d76401fedc1d494bfce6673aa7012d8a8
lnqbat <b.lonqueu98@gmail.com>
2025-06-17 11:01:15 +0200

    refactor: update HBnBFacade to use user_repository consistently

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -2,22 +2,25 @@ from app.persistence.repository import InMemoryRepository
 from app.models.user import User
 
 class HBnBFacade:
-    def __init__(self):
-        self.user_repo = InMemoryRepository()
+    def __init__(self, user_repository=None, place_repository=None, review_repository=None, amenity_repository=None):
+        self.user_repository = user_repository
+        self.place_repository = place_repository
+        self.review_repository = review_repository
+        self.amenity_repository = amenity_repository
 
     def create_user(self, user_data):
         user = User(**user_data)
-        self.user_repo.add(user)
+        self.user_repository.add(user)
         return user
 
     def get_user(self, user_id):
-        return self.user_repo.get(user_id)
+        return self.user_repository.get(user_id)
 
     def get_user_by_email(self, email):
-        return self.user_repo.get_by_attribute('email', email)
+        return self.user_repository.get_by_attribute('email', email)
 
     def update_user(self, user_id, update_data):
-        user = self.user_repo.get(user_id)
+        user = self.user_repository.get(user_id)
         if not user:
             return None
 
@@ -25,6 +28,7 @@ class HBnBFacade:
             if hasattr(user, key):
                 setattr(user, key, value)
 
-        self.user_repo.save(user)
         return user
 
+    def get_all_users(self):
+        return self.user_repository.get_all()
