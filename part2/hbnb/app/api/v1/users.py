589db6c870d443656c2c6e77e395692c9a5addf6
lnqbat <b.lonqueu98@gmail.com>
2025-06-30 10:54:00 +0200

    fix: add error response for existing email during user registration

--- part2/hbnb/app/api/v1/users.py
+++ part2/hbnb/app/api/v1/users.py
@@ -23,13 +23,14 @@ class UserList(Resource):
             return {'error': 'Email already registered'}, 400
 
         new_user = facade.create_user(user_data)
+        new_user.hash_password(user_data['password'])
         return {
             'id': new_user.id,
             'first_name': new_user.first_name,
             'last_name': new_user.last_name,
             'email': new_user.email
         }, 201
-
+ 
     @api.response(200, 'List of users retrieved successfully')
     def get(self):
         """ Get a list of all users """
