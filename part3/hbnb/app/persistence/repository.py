61f3fd3f72133e8f6a6070b8b28bc517bd4d086e
lnqbat <b.lonqueu98@gmail.com>
2025-07-03 13:48:38 +0200

    feat: implement SQLAlchemy repository for database operations

--- part3/hbnb/app/persistence/repository.py
+++ part3/hbnb/app/persistence/repository.py
@@ -1,3 +1,4 @@
+from app import db
 from abc import ABC, abstractmethod
 
 class Repository(ABC):
@@ -63,3 +64,33 @@ class InMemoryRepository(Repository):
 class AmenityRepository(InMemoryRepository):
     def __init__(self):
         super().__init__()
+
+class SQLAlchemyRepository(Repository):
+    def __init__(self, model):
+        self.model = model
+
+    def add(self, obj):
+        db.session.add(obj)
+        db.session.commit()
+
+    def get(self, obj_id):
+        return self.model.query.get(obj_id)
+
+    def get_all(self):
+        return self.model.query.all()
+
+    def update(self, obj_id, data):
+        obj = self.get(obj_id)
+        if obj:
+            for key, value in data.items():
+                setattr(obj, key, value)
+            db.session.commit()
+
+    def delete(self, obj_id):
+        obj = self.get(obj_id)
+        if obj:
+            db.session.delete(obj)
+            db.session.commit()
+
+    def get_by_attribute(self, attr_name, attr_value):
+        return self.model.query.filter(getattr(self.model, attr_name) == attr_value).first()
