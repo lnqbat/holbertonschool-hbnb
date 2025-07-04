0c316c5b090ff833dfdab4b621cdfcb5e09e9255
lnqbat <b.lonqueu98@gmail.com>
2025-07-04 14:55:12 +0200

    refactor: reorganize User model attributes and clean up initialization

--- part3/hbnb/app/models/user.py
+++ part3/hbnb/app/models/user.py
@@ -1,11 +1,17 @@
 import re
+from app import db, bcrypt
 from app.models import BaseModel
 
 class User(BaseModel):
+    __tablename__ = 'users'
+
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=True)
+    is_admin = db.Column(db.Boolean, default=False)
+
     def __init__(self, first_name, last_name, email, password=None, hashed=False):
-        """
-        Users class.
-        """
         super().__init__()
 
         if not first_name or len(first_name) > 50:
@@ -19,7 +25,6 @@ class User(BaseModel):
         self.last_name = last_name
         self.email = email
         self.is_admin = False
-        self.places = []
 
         if password:
             if hashed:
@@ -27,7 +32,7 @@ class User(BaseModel):
             else:
                 self.hash_password(password)
         else:
-            self.password=None
+            self.password = None
 
     def _is_valid_email(self, email):
         regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
@@ -42,9 +47,7 @@ class User(BaseModel):
         }
 
     def hash_password(self, password):
-        from app import bcrypt
         self.password = bcrypt.generate_password_hash(password).decode('utf-8')
 
     def verify_password(self, password):
-        from app import bcrypt
         return bcrypt.check_password_hash(self.password, password)
