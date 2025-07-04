bb813d15be3d9296c48ef0299538ec721d276e8d
lnqbat <b.lonqueu98@gmail.com>
2025-07-04 14:55:26 +0200

    refactor: simplify HBnBFacade initialization by removing unnecessary repository parameters

--- part3/hbnb/app/services/__init__.py
+++ part3/hbnb/app/services/__init__.py
@@ -1,9 +1,4 @@
 from app.services.facade import HBnBFacade
 from app.persistence.repository import InMemoryRepository, AmenityRepository
 
-facade = HBnBFacade(
-    user_repository=InMemoryRepository(),
-    place_repository=InMemoryRepository(),
-    review_repository=InMemoryRepository(),
-    amenity_repository=AmenityRepository()
-)
+facade = HBnBFacade()
