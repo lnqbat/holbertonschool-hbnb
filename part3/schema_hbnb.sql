e5ebc7a9a578be0cc7f7d12c04720c32f4853fc2
Uyed Debara <iyeddebara@icloud.com>
2025-07-07 12:20:49 +0200

    feat: create initial database schema with users, places, reviews, amenities, and place_amenity tables

---
+++ part3/schema_hbnb.sql
@@ -0,0 +1,58 @@
+CREATE TABLE users (
+    id CHAR(36) PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    is_admin BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE places (
+    id CHAR(36) PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    price DECIMAL(10, 2) NOT NULL,
+    latitude FLOAT NOT NULL,
+    longitude FLOAT NOT NULL,
+    owner_id CHAR(36) NOT NULL,
+    FOREIGN KEY (owner_id) REFERENCES users(id)
+);
+
+CREATE TABLE reviews (
+    id CHAR(36) PRIMARY KEY,
+    text TEXT NOT NULL,
+    rating INT CHECK(rating >= 1 AND rating <= 5) NOT NULL,
+    user_id CHAR(36) NOT NULL,
+    place_id CHAR(36) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (place_id) REFERENCES places(id),
+    UNIQUE(user_id, place_id)
+);
+
+CREATE TABLE amenities (
+    id CHAR(36) PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE place_amenity (
+    place_id CHAR(36),
+    amenity_id CHAR(36),
+    PRIMARY KEY (place_id, amenity_id),
+    FOREIGN KEY (place_id) REFERENCES places(id),
+    FOREIGN KEY (amenity_id) REFERENCES amenities(id)
+);
+
+
+INSERT INTO users (id, first_name, last_name, email, password, is_admin) VALUES (
+    '36c9050e-ddd3-4c3b-9731-9f487208bbc1', 
+    'Admin', 
+    'HBnB', 
+    'admin@hbnb.io', 
+    '$2b$12$1234567890abcdef1234567890abcdef1234567890abcdef123456', -- Remplace ce hash par le hash réel généré
+    TRUE
+);
+
+INSERT INTO amenities (id, name) VALUES
+('f14fb2ac-3b42-4ac5-8270-564ed4523d0a', 'WiFi'),
+('96eaad60-348e-412a-842b-9c18bb7c2f7e', 'Swimming Pool'),
+('64ae9d88-6fbd-41d4-b21c-9eb9e994a94d', 'Air Conditioning');
